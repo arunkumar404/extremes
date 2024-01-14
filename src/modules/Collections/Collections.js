2acaeacd04eba83a19ade76fc8cda08fe7aa9f10
Arun kumar <aravi@purecodesoftware.com>
2024-01-14 15:59:21 +0530

    colelctions page filters added

--- src/modules/Collections/Collections.js
+++ src/modules/Collections/Collections.js
@@ -1,18 +1,41 @@
 import React from 'react'
 import styles from './Collections.module.css'
 import bg_img_2 from '../../common/images/bg_img_2.jpg'
-import { useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom'
+import { FilterIcon, RightArrowIcon } from '../../common/icons'
 
 const Collections = () => {
-  const {category} = useParams()
-  
+  const { category } = useParams()
+
   return (
     <div className={styles.collections_container}>
       <div
         className={styles.topBgPic}
-        style={{ backgroundImage: `url(${bg_img_2})`, minHeight:'400px' }}
+        style={{ backgroundImage: `url(${bg_img_2})` }}
       >
-        <p style={{color: '#ffffff', fontSize: '4rem', fontWeight:400}}>Ready-to-Wear</p>
+        <p style={{ color: '#ffffff', fontSize: '4rem', fontWeight: 400 }}>
+          Ready-to-Wear
+        </p>
+      </div>
+      <div className={styles.filterNsort}>
+        <div
+          className={styles.btnIconContainer}
+          style={{ borderRight: '1px solid #5a5959' }}
+        >
+          <button>FILTER</button>
+          <div className={styles.iconDiv}>
+            <FilterIcon />
+          </div>
+        </div>
+        <div
+          className={styles.btnIconContainer}
+          style={{ borderLeft: '1px solid #5a5959' }}
+        >
+          <button>SORT</button>
+          <div className={styles.iconDiv}>
+            <RightArrowIcon />
+          </div>
+        </div>
       </div>
     </div>
   )
